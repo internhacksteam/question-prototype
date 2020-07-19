292bf2c1fb46d3a21e2dd2a12dd3ee9e25b96eee
Kristen <kristenmarcinek@protonmail.com>
2020-07-19 18:26:17 -0400

    API and updated Flask

--- main.py
+++ main.py
@@ -12,22 +12,35 @@ def output():
         answer = request.form.get('Answer')
 
         q1answers = {
-            "Kristen",
-            "kristen",
-            "Niveda",
-            "niveda",
-            "Isabel",
-            "isabel",
-            "Crystal",
-            "crystal",
-            "Ashley",
-            "ashley"
+            'print("Hello World!")',
+            'print("Hello world!")',
+            'print("hello world!")',
+            'print("hello World!")'
         }
 
         if answer in q1answers:
             return render_template('output.html', response="Correct!")
         else:
             return render_template('output.html', response="Sorry, try again!")
+
+@app.route('/q2/')
+def input_2():
+    return render_template('q2.html')
+
+@app.route('/q2/', methods=['POST', 'GET'])
+def output_2():
+    if request.method == 'POST':
+        answer = request.form.get('Answer')
+
+        q2answers = {
+            '[box1 = “Divergent”]',
+            '[box1 = “divergent”]'
+        }
+
+        if answer in q2answers:
+            return render_template('output_q2.html', response="Correct!")
+        else:
+            return render_template('output_q2.html', response="Sorry, try again!")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug='true')
