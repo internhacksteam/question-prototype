861adcbc8b959c1b264a85ad0c0c4e495f12c8bd
Kristen <61288999+kirbong@users.noreply.github.com>
2020-07-22 16:09:24 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/questionstart-page')
 def input():
     return render_template('index.html')
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/questionstart-page', methods=['POST', 'GET'])
 def output():
     if request.method == 'POST':
         answer = request.form.get('Answer')
@@ -19,28 +19,10 @@ def output():
         }
 
         if answer in q1answers:
-            return render_template('output.html', response="Correct!")
+            return {'response': "Correct!")
         else:
-            return render_template('output.html', response="Sorry, try again!")
+            return {'response': "Sorry, try again!")
 
-@app.route('/q2/')
-def input_2():
-    return render_template('q2.html')
-
-@app.route('/q2/', methods=['POST', 'GET'])
-def output_2():
-    if request.method == 'POST':
-        answer = request.form.get('Answer')
-
-        q2answers = {
-            '[box1 = “Divergent”]',
-            '[box1 = “divergent”]'
-        }
-
-        if answer in q2answers:
-            return render_template('output_q2.html', response="Correct!")
-        else:
-            return render_template('output_q2.html', response="Sorry, try again!")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug='true')
